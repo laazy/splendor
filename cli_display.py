afd5e4d7d1602cb281809cfb60f35fdf0df273be
laazy <616408874@qq.com>
2023-06-25 00:59:04 +0800

    ♿ add more readable cli output

--- cli_display.py
+++ cli_display.py
@@ -1,22 +1,23 @@
-from game_basic import Deck, DevelopCard, GemSet, Player
+from game_basic import Deck, DevelopCard, GemSet, Player, Gem
+from player_action import Action
 
 
 
-def GemSet__str__(self) -> str:
+def GemSet__str__(self: GemSet) -> str:
     return ' '.join(
-        [f'{gem.value}{cnt}' for gem, cnt in self.coins.items() if cnt > 0]
+        [f'{gem}{cnt}' for gem, cnt in self.coins.items() if cnt > 0]
     )
     
 
-def DevelopCard__str__(self):
-    return f'{self.gem.value}: {self.cost}  [{self.score}]'
+def DevelopCard__str__(self: DevelopCard):
+    return f'{self.gem}: {self.cost}  [{self.score}]'
 
 
-def Player__str__(self) -> str:
-    return f'Player {self.name}:\n      coins: {self.coins}\n      score: {self.score}\n     cards: {self.cards_gem}\n reserved: {self.reserved}'
+def Player__str__(self: Player) -> str:
+    return f'Player {self.name}:\n      coins: {self.coins}\n      score: {self.score}\n      cards: {self.cards_gem}\n reserved:\n'+"\n".join(map(str, self.reserved))
 
 
-def Deck__str__(self) -> str:
+def Deck__str__(self: Deck) -> str:
     return (
         f'============ round: {self.round} ============\n'
         + '\n'.join(map(str, self.players))
@@ -32,9 +33,17 @@ def Deck__str__(self) -> str:
         + str(self.coins)
         + f'\n============ {self.current_player.name} turn ============\n'
     )
+    
+def Gem__str__(self: Gem) -> str:
+    return self.value
+
+def Action__str__(self: Action) -> str:
+    return str(self.value)
 
 def add_cli_display():
     Player.__str__ = Player__str__
     DevelopCard.__str__ = DevelopCard__str__
     GemSet.__str__ = GemSet__str__
     Deck.__str__ = Deck__str__
+    Gem.__str__ = Gem__str__
+    Action.__str__ = Action__str__
